8065669107f76967a66eaae75337ad71f11ed72d
cervatz <cervatz@gmail.com>
2013-10-21 14:10:20 +0200

    Fixing typo.

---
+++ server/controllers/clickout-controller-module.js
@@ -0,0 +1,9 @@
+module.exports.ClickoutController = ClickoutController
+
+function ClickoutController() {}
+
+ClickoutController.prototype.getAllClickouts = function() {
+	console.log("ClickoutController.propotype.getAllClickouts")
+}
+
+
